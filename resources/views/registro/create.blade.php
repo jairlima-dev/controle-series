986cbfb7faa09b3370629be8ad8f161b505d0a11
Jair Lima <jairlimamede@hotmail.com>
2021-04-14 17:22:44 -0300

    Alter /registro/create

--- resources/views/registro/create.blade.php
+++ resources/views/registro/create.blade.php
@@ -6,27 +6,29 @@
 
 @section('conteudo')
 
-    @include('errors')
+@include('mensagem')
 
-    <form method="post">
-        @csrf
-        <div class="form-group">
-            <label for="nome">Nome</label>
-            <input type="text" name="name" id="nome" required class="form-control">
-        </div>
+@include('errors')
 
-        <div class="form-group">
-            <label for="email">E-mail</label>
-            <input type="email" name="email" id="email" required class="form-control">
-        </div>
+<form method="post">
+    @csrf
+    <div class="form-group">
+        <label for="nome">Nome</label>
+        <input type="text" name="name" id="nome" required class="form-control">
+    </div>
 
-        <div class="form-group">
-            <label for="password">Senha</label>
-            <input type="password" name="password" id="password" required min="1" class="form-control">
-        </div>
+    <div class="form-group">
+        <label for="email">E-mail</label>
+        <input type="email" name="email" id="email" required class="form-control">
+    </div>
 
-        <button type="submit" class="btn btn-primary mt-3">
-            Entrar
-        </button>
-    </form>
+    <div class="form-group">
+        <label for="password">Senha</label>
+        <input type="password" name="password" id="password" required min="1" class="form-control">
+    </div>
+
+    <button type="submit" class="btn btn-primary mt-3">
+        Entrar
+    </button>
+</form>
 @endsection
